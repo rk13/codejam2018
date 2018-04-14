8aa81d872b8df9bf61af8ab806458735120ccac7
Vladimir Kotov <vladimir.kotov@citadele.lv>
2018-04-15 00:34:52 +0300

    A2 initial solution

--- a2/src/Solution.java
+++ a2/src/Solution.java
@@ -1,89 +1,81 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution {
 
+    static Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
     static int R, B, C;
-    static int[][] CS;
 
-    static long MIN;
+    static int[][] F;
 
+    static long RESULT = 0;
 
     public static void main(String[] args) {
-        System.out.println(Integer.MAX_VALUE);
-    }
-    public static void main1(String[] args) {
-        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-        int t = Integer.valueOf(in.nextLine());
+        int t = Integer.parseInt(in.nextLine());
         for (int i = 1; i <= t; i++) {
             String[] s = in.nextLine().split(" ");
-
             R = Integer.parseInt(s[0]);
             B = Integer.parseInt(s[1]);
             C = Integer.parseInt(s[2]);
-            MIN = Integer.MAX_VALUE;
 
-            CS = new int[C][3];
+            F = new int[C][];
             for (int j = 0; j < C; j++) {
-                CS[j] = Stream.of(in.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+                String[] s1 = in.nextLine().split(" ");
+                F[j] = Stream.of(s1).mapToInt(Integer::parseInt).toArray();
             }
 
-            //process();
-
-            System.out.println("Case #" + i + ": " + MIN);
+            String r = process();
+            System.out.println("Case #" + i + ": " + r);
         }
     }
 
-//    private static int process() {
-//
-//        Set<Integer> availableC = new HashSet<>();
-//        for (int i = 0; i < c; i++) {
-//            availableC.add(i);
-//        }
-//
-//        find(R, B, availableC, 0);
-//        return min;
-//    }
-//
-//
-//    private static void find(int r, int b, Set<Integer> availableC, int sum) {
-//        if (b <= 0) {
-//            if (min > sum) {
-//                min = sum;
-//            }
-//            return;
-//        }
-//
-//        if (r <= 0) {
-//            return;
-//        }
-//
-//        if (sum > min) {
-//            return;
-//        }
-//
-//        for (int i = 1; i <= b; i++) {
-//
-//            for (int j = 0; j < C; j++) {
-//
-//                if (availableC.contains(j) && i <= cs[j][0]) {
-//
-//                    //for (int k = 1; k <= Math.min(i, cs[j][0]); k++) {
-//
-//                    int sum2 = Math.max(sum, (i * cs[j][1] + cs[j][2]));
-//                    availableC.remove(j);
-//
-//                    find(r - 1, b - i, availableC, sum2);
-//
-//                    availableC.add(j);
-//                    //}
-//                }
-//            }
-//        }
-//    }
+    public static String process() {
+        RESULT = Long.MAX_VALUE;
+        find(Math.min(R, C), B, IntStream.range(0, C).boxed().collect(Collectors.toSet()), 0);
+        return "" + RESULT;
+    }
 
+    private static void find(int r, int b, Set<Integer> freeCachiers, long max) {
+
+        if (b == 0) {
+            RESULT = Math.min(RESULT, max);
+            return;
+        }
+
+        if (r == 0) {
+            return;
+        }
+
+        if (max >= RESULT) {
+            return;
+        }
+
+        for (int bits = 1; bits <= b; bits++) {
+            for (int cachier = 0; cachier < C; cachier++) {
+
+                if (freeCachiers.contains(cachier)) {
+
+                    if (F[cachier][0] >= bits) {
+
+                        long spent = F[cachier][1] * bits + F[cachier][2];
+
+                        freeCachiers.remove(cachier);
+
+                        find(r - 1, b - bits, freeCachiers, Math.max(spent, max));
+
+                        freeCachiers.add(cachier);
+                    }
+                }
+            }
+        }
+    }
 }
+
+

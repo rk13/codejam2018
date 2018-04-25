b28c28747d7f2dc968b15b52d9e5edbf7f8828b5
Vladimir Kotov <vladimir.kotov@citadele.lv>
2018-04-26 01:03:21 +0300

    Fixed a2

--- a2/src/Solution.java
+++ a2/src/Solution.java
@@ -1,81 +1,71 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution {
 
-    static Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+    static long R, B;
 
-    static int R, B, C;
+    static int C;
 
-    static int[][] F;
+    static long[][] F;
 
-    static long RESULT = 0;
 
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
         int t = Integer.parseInt(in.nextLine());
+
         for (int i = 1; i <= t; i++) {
             String[] s = in.nextLine().split(" ");
-            R = Integer.parseInt(s[0]);
-            B = Integer.parseInt(s[1]);
+            R = Long.parseLong(s[0]);
+            B = Long.parseLong(s[1]);
             C = Integer.parseInt(s[2]);
 
-            F = new int[C][];
+            F = new long[C][];
             for (int j = 0; j < C; j++) {
                 String[] s1 = in.nextLine().split(" ");
-                F[j] = Stream.of(s1).mapToInt(Integer::parseInt).toArray();
+                F[j] = Stream.of(s1).mapToLong(Long::parseLong).toArray();
             }
 
-            String r = process();
+            long r = process();
             System.out.println("Case #" + i + ": " + r);
         }
     }
 
-    public static String process() {
-        RESULT = Long.MAX_VALUE;
-        find(Math.min(R, C), B, IntStream.range(0, C).boxed().collect(Collectors.toSet()), 0);
-        return "" + RESULT;
-    }
+    public static long process() {
+        long hi = Long.MAX_VALUE;
+        long lo = 0;
 
-    private static void find(int r, int b, Set<Integer> freeCachiers, long max) {
-
-        if (b == 0) {
-            RESULT = Math.min(RESULT, max);
-            return;
-        }
-
-        if (r == 0) {
-            return;
-        }
-
-        if (max >= RESULT) {
-            return;
-        }
-
-        for (int bits = 1; bits <= b; bits++) {
-            for (int cachier = 0; cachier < C; cachier++) {
-
-                if (freeCachiers.contains(cachier)) {
-
-                    if (F[cachier][0] >= bits) {
-
-                        long spent = F[cachier][1] * bits + F[cachier][2];
-
-                        freeCachiers.remove(cachier);
-
-                        find(r - 1, b - bits, freeCachiers, Math.max(spent, max));
-
-                        freeCachiers.add(cachier);
-                    }
-                }
+        while (hi > lo) {
+            long mid = (hi + lo) / 2;
+            if (func(mid)) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
             }
         }
+        return hi;
+    }
+
+    private static boolean func(long t) {
+        Long x[] = new Long[C];
+        for (int i = 0; i < C; i++) {
+            double floor = Math.max(Math.floor((t - F[i][2]) / F[i][1]), 0);
+            x[i] = (long) Math.min(floor, F[i][0]);
+        }
+
+        Arrays.sort(x, Collections.reverseOrder());
+
+        long sum = 0;
+        for (int i = 0; i < R; i++) {
+            sum += x[i];
+        }
+        return sum >= B;
     }
 }
-
 

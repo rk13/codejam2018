38da78f5f6128e66251b091a3502f0928f051ecf
Vladimir Kotov <vladimir.kotov@citadele.lv>
2018-04-29 21:34:07 +0300

    Stopped

--- b1/src/Solution.java
+++ b1/src/Solution.java
@@ -1,41 +1,110 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Objects;
-import java.util.Scanner;
+import java.math.BigDecimal;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Solution {
 
     static Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
 
-    static int X, Y;
+    static int N, L;
 
-    static int[] R;
+    static int[] C;
 
-    static int[][] F;
 
     public static void main(String[] args) {
         int t = Integer.parseInt(in.nextLine());
         for (int i = 1; i <= t; i++) {
             String[] s = in.nextLine().split(" ");
-            X = Integer.parseInt(s[0]);
-            Y = Integer.parseInt(s[1]);
+            N = Integer.parseInt(s[0]);
+            L = Integer.parseInt(s[1]);
 
-//            String[] s1 = in.nextLine().split(" ");
-//            R = Stream.of(s1).mapToInt(Integer::parseInt).toArray();
-//
-//            F = new int[Y][];
-//            for (int j = 0; j < Y; j++) {
-//                String s2 = in.nextLine();
-//                F[j] = s2.chars().map(x -> (char) x == '*' ? 1 : 0).toArray();
-//            }
+            String[] s1 = in.nextLine().split(" ");
+            C = Stream.of(s1).mapToInt(Integer::parseInt).toArray();
 
-            String r = process();
+            long r = process();
             System.out.println("Case #" + i + ": " + r);
         }
     }
 
-    public static String process() {
-        return X + " " + Y;
+
+    static int[] langs;
+    static int[] copy;
+    static long iters;
+
+    public static long process() {
+
+         langs = new int[N];
+         copy = new int[N];
+        int answered = 0;
+        iters = 0;
+
+        for (int i = 0; i < C.length; i++) {
+            langs[i] = C[i];
+            answered += C[i];
+        }
+
+        int remains = N - answered;
+
+
+        return next(remains, 0);
+
     }
+
+    private static long next(int remains, int language) {
+        iters++;
+
+        if (iters > 5000000) {
+            return 100;
+        }
+        if (remains <= 0) {
+            return percents(langs);
+        }
+
+        if (language == N) {
+            return 0;
+        }
+
+        long max = 0;
+
+        for (int i = 0; i <= remains; i++) {
+            langs[language] += i;
+            long tmp = next(remains - i, language + 1);
+            langs[language] -= i;
+            if (tmp > max) {
+                max = tmp;
+            }
+        }
+
+        return max;
+    }
+
+
+    static Map<String, Long> cached = new HashMap<>();
+
+    public static long percents(int[] votes) {
+
+        System.arraycopy(votes, 0, copy, 0, votes.length);
+        Arrays.sort(copy);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < copy.length; i++) {
+            sb.append(copy[i]).append(',');
+        }
+        String ss = sb.toString();
+        if (cached.containsKey(ss)) {
+            return cached.get(ss);
+        }
+
+        long sum = 0;
+        for (int i = 0; i < votes.length; i++) {
+            long pct = Math.round(((double) votes[i] * 100) / N);
+            sum += pct;
+        }
+
+        cached.put(ss,sum);
+        return sum;
+    }
+
+
 }
--- b2/src/Solution.java
+++ b2/src/Solution.java
@@ -1,40 +1,83 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Solution {
 
     static Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
 
-    static int X, Y;
+    static int S;
 
-    static int[] R;
+    static int[] D;
+    static int[] A;
+    static int[] B;
+    static int[] M;
+    static int[] N;
 
-    static int[][] F;
 
     public static void main(String[] args) {
         int t = Integer.parseInt(in.nextLine());
         for (int i = 1; i <= t; i++) {
             String[] s = in.nextLine().split(" ");
-            X = Integer.parseInt(s[0]);
-            Y = Integer.parseInt(s[1]);
+            S = Integer.parseInt(s[0]);
 
-//            String[] s1 = in.nextLine().split(" ");
-//            R = Stream.of(s1).mapToInt(Integer::parseInt).toArray();
-//
-//            F = new int[Y][];
-//            for (int j = 0; j < Y; j++) {
-//                String s2 = in.nextLine();
-//                F[j] = s2.chars().map(x -> (char) x == '*' ? 1 : 0).toArray();
-//            }
+            D = new int[S];
+            A = new int[S];
+            B = new int[S];
+            M = new int[S];
+            N = new int[S];
+
+            for (int j = 0; j < S; j++) {
+                String[] s1 = in.nextLine().split(" ");
+                D[j] = Integer.parseInt(s1[0]);
+                A[j] = Integer.parseInt(s1[1]);
+                B[j] = Integer.parseInt(s1[2]);
+                M[j] = D[j] + A[j];
+                N[j] = D[j] - B[j];
+
+            }
 
             String r = process();
             System.out.println("Case #" + i + ": " + r);
         }
     }
 
+
+    static int longest = 0;
+    static int longestVal = 0;
+
     public static String process() {
-        return X + " " + Y;
+
+        longest = 0;
+        longestVal = 0;
+
+        for (int i = 0; i < S; i++) {
+
+            boolean flag = true;
+            int end = i;
+
+            while(flag && end < S) {
+                flag = (M[end] == M[i]) || (N[end] == N[i]);
+                end++;
+            }
+
+            int len = end - i;
+            if (!flag) len--;
+
+            if (longest < len) {
+                longest = len;
+                longestVal = 1;
+            } else if (longest == len) {
+                longestVal++;
+            }
+
+        }
+
+        return longest + " " + longestVal;
     }
+
+
+
+
 }

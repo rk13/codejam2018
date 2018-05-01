8d7c5fe4ed71143e2ce4f9e8bd284e47f25cfa40
Vladimir Kotov <vladimir.kotov@citadele.lv>
2018-05-01 23:09:50 +0300

    b1 small

--- b1/src/Solution.java
+++ b1/src/Solution.java
@@ -22,11 +22,69 @@ public class Solution {
             C = Stream.of(s1).map(Integer::parseInt).collect(Collectors.toList());
             C.sort(Collections.reverseOrder());
 
-            Set<List<Integer>> p = partitions(N);
-            long r = process(p);
+            int[] a;
+            Arrays.stream(a).sum()
 
-            System.out.println("Case #" + i + ": " + r);
+
+            knapsack();
+
+            //long r = process();
+
+            System.out.println("Case #" + i + ": " + "");
         }
+    }
+
+
+    public static int knapsack() {
+
+        // We can first handle the base case of the function.
+        // We can easily compute f(1, b) since there is only at most one satisfying partition.
+        // Therefore, f(1, b) = round(b / N × 100) if b ≥ C1, or -∞ otherwise.
+
+
+        int[] CC = new int[N];
+        for (int i = 0; i < C.size(); i++) {
+            CC[i] = C.get(i);
+        }
+
+
+        int[][] F = new int[N + 1][N + 1]; //Create a matrix. Items are in rows and weight at in columns +1 on each side
+
+        for (int i = 1; i <= N; i++) {
+            if (i >= CC[1])
+                F[1][i] = (int) Math.round(((double) i / N) * 100);
+            else
+                F[1][i] = Integer.MAX_VALUE;
+
+        }
+
+        // for a > 1, f(a, b) = max (Ca ≤ i ≤ b) (round(i / N × 100) + f(a - 1, b - i)).
+
+        for (int item = 1; item <= N; item++) {
+            for (int weight = 1; weight <= N; weight++) {
+                //Is the current items weight less than or equal to running weight
+                if (wt[item - 1] <= weight) {
+                    //Given a weight, check if the value of the current item + value of the item that we could afford with the remaining weight
+                    //is greater than the value without the current item itself
+                    int valWithoutCurrent = V[item - 1][weight];
+                    int valCurrentAndMaxForRemainingWeight = val[item - 1] + V[item - 1][weight - wt[item - 1]];
+                    V[item][weight] = Math.max(valCurrentAndMaxForRemainingWeight, valWithoutCurrent);
+                } else {
+                    //If the current item's weight is more than the running weight, just carry forward the value without the current item
+                    V[item][weight] = V[item - 1][weight];
+                }
+            }
+        }
+
+//        //Printing the matrix
+//        for (int[] rows : V) {
+//            for (int col : rows) {
+//                System.out.format("%5d", col);
+//            }
+//            System.out.println();
+//        }
+//        return V[N][W];
+        return 0;
     }
 
     private static Set<List<Integer>> partitions(Integer n) {

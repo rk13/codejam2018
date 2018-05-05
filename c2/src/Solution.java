9c98c82f9aad89e37302dd287e07172ff3fb8681
Vladimir Kotov <vladimir.kotov@citadele.lv>
2018-05-05 23:42:49 +0300

    C3 solution

--- c2/src/Solution.java
+++ c2/src/Solution.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
@@ -11,6 +12,7 @@ public class Solution {
 
     static int[] WT;
 
+    static boolean DEBUG;
 
     public static void main(String[] args) {
         int t = Integer.parseInt(in.nextLine());
@@ -20,12 +22,6 @@ public class Solution {
 
             String[] s1 = in.nextLine().split(" ");
             WT = Stream.of(s1).mapToInt(Integer::parseInt).toArray();
-//
-//            F = new int[Y][];
-//            for (int j = 0; j < Y; j++) {
-//                String s2 = in.nextLine();
-//                F[j] = s2.chars().map(x -> (char) x == '*' ? 1 : 0).toArray();
-//            }
 
             String r = process();
             System.out.println("Case #" + i + ": " + r);
@@ -34,87 +30,58 @@ public class Solution {
 
     public static String process() {
 
-        int W = WT[N - 1] * 6;
+        // We define a function f(x, y) as the maximum number of ants that can form a stack
+        // (following the stack requirements given in the problem statement),
+        // in which we only consider ants from the first ant to the x-th ant,
+        // inclusive, and the sum of the ants' weight is not more than y.
 
-        long[] v = new long[N];
-
-        for (int i = WT.length - 1; i >= 0; i--) {
-            v[i] = WT[i] * 6;
-        }
+        int W = Arrays.stream(WT).sum();
 
         int[][] V = new int[N + 1][W + 1];
-        int[][] X = new int[N + 1][W + 1];
 
-        for (int i = 1; i <= W; i++) {
-            if (i >= WT[0]) {
-                V[1][i] = 1;
-                X[1][i] = WT[0];
-            } else {
-                V[1][i] = 0;
-                X[1][i] = 0;
+        for (int weight = 1; weight <= W; weight++) {
+            if (WT[0] <= weight) {
+                V[1][weight] = 1;
             }
         }
 
         for (int ant = 2; ant <= N; ant++) {
             for (int weight = 1; weight <= W; weight++) {
-                boolean done = false;
+                int valWithoutCurrent = V[ant - 1][weight];
                 if (WT[ant - 1] <= weight) {
-                    int wremaining = weight - WT[ant - 1];
-
-                    if (X[ant - 1][wremaining] <= v[ant - 1]) {
-                        int valCurrentAndMaxForRemainingWeight = 1 + V[ant - 1][wremaining];
-                        int valWithoutCurrent = V[ant - 1][weight];
-
-                        if (valCurrentAndMaxForRemainingWeight > valWithoutCurrent) {
-                            V[ant][weight] = valCurrentAndMaxForRemainingWeight;
-                            X[ant][weight] = X[ant - 1][wremaining] + WT[ant - 1];
-                            done = true;
-                        }
-                    }
-                }
-
-                if (!done) {
-                    V[ant][weight] = V[ant - 1][weight];
-                    X[ant][weight] = X[ant - 1][weight];
+                    int weightRemaining = Math.min(weight - WT[ant - 1], WT[ant - 1] * 6);
+                    int valCurrentAndMaxForRemainingWeight = V[ant - 1][weightRemaining] + 1;
+                    V[ant][weight] = Math.max(valCurrentAndMaxForRemainingWeight, valWithoutCurrent);
+                } else {
+                    V[ant][weight] = valWithoutCurrent;
                 }
             }
         }
 
-//////        //Printing the matrix
-//        for (int[] rows : V) {
-//            for (int col : rows) {
-//                System.out.format("%5d", col);
-//            }
-//            System.out.println();
-//        }
-//
-//
-//        System.out.println();
-//
-//        for (int[] rows : X) {
-//            for (int col : rows) {
-//                System.out.format("%5d", col);
-//            }
-//            System.out.println();
-//        }
+        if (DEBUG) {
+            for (int[] rows : V) {
+                for (int col : rows) {
+                    System.out.format("%5d", col);
+                }
+                System.out.println();
+            }
+        }
 
-//        int max = Integer.MIN_VALUE;
-//        for (int i = 1; i <= W; i++) {
-//            max = Math.max(max, V[N][i]);
-//        }
         return V[N][W] + "";
-
     }
 
 
     public static void main1(String[] args) {
-//        // N = 3;
-//        // WT  = new int[]{8, 4, 100};
+        DEBUG = true;
+
+//         N = 3;
+//         WT  = new int[]{8, 4, 100};
+
 //        N = 2;
 //        WT = new int[]{9, 1};
 
         N = 9;
-        WT = new int[]{10, 10, 10, 10, 10, 5, 5, 5, 100};
+        WT = new int[]{10, 10, 10, 10, 10, 10, 10, 10, 100};
 
         process();
     }
